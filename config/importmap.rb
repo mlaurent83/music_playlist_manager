7ca6e6cad4c6ff62c125aba124d39882c14330e3
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-10-10 16:05:20 +0100

    modified importmap to point to react

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin 'application', to: 'application.js'
-pin 'react'
-pin 'react-dom'
+pin "react", to: "https://cdn.jsdelivr.net/npm/react@18/umd/react.production.min.js"
+pin "react-dom", to: "https://cdn.jsdelivr.net/npm/react-dom@18/umd/react-dom.production.min.js"
+
 pin 'fontawesome', to: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
